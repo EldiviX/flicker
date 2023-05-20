edb5c13c3d07308a3c96a9e914ee49088e0f3e4e
Lauren Flow <81534306+ViJi-228@users.noreply.github.com>
2023-05-21 00:02:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let butt = document.getElementById('change-bg');
+
+function setColor() {
+    let coll = setInterval(function() {
+        const colors = ['red', 'blue', 'green', 'orange'];
+        let randomColor = colors[Math.floor(Math.random() * colors.length)];
+        document.body.style.backgroundColor = randomColor
+        
+    }, 500)
+     
+    
+}
+
+
+butt.addEventListener('click', setColor);
